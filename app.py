265fdcababd0fb0f7afd28c8cce82aa241f4d6af
Pulkit12dhingra <pulkit12dhingra@gmail.com>
2020-12-20 15:17:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -310,47 +310,49 @@ def image(data_image):
         # decode and convert into image
 
         if chara["t"]!=0:
-            pimg = Image.open(b)
+            try:
+                pimg = Image.open(b)
             ## converting RGB to BGR, as opencv standards
-            frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
-            #frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_BGR2GRAY)
-            # Process the image frame
-            gray = cv2.cvtColor(np.array(frame), cv2.COLOR_BGR2GRAY)
-            hand = handDetect.detectMultiScale(gray, 1.3, 5)
-            for (x, y, w, h) in hand:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                center_point = center(x, y, w, h)
-                chara["l"].append(center_point)
-                tracking(frame, chara["l"])
+                frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+                #frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_BGR2GRAY)
+                # Process the image frame
+                gray = cv2.cvtColor(np.array(frame), cv2.COLOR_BGR2GRAY)
+                hand = handDetect.detectMultiScale(gray, 1.3, 5)
+                for (x, y, w, h) in hand:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                    center_point = center(x, y, w, h)
+                    chara["l"].append(center_point)
+                    tracking(frame, chara["l"])
 
-            frame = imutils.resize(frame, width=700)
-            frame = cv2.flip(frame, 1)
-            mins, secs = divmod(chara["t"], 60)
-            timer = '{:02d}:{:02d}'.format(mins, secs)
-            cv2.putText(frame, timer, org, font,
-                        fontScale, color, thickness, cv2.LINE_AA)
-            imgencode = cv2.imencode('.jpg', frame)[1]
+                frame = imutils.resize(frame, width=700)
+                frame = cv2.flip(frame, 1)
+                mins, secs = divmod(chara["t"], 60)
+                timer = '{:02d}:{:02d}'.format(mins, secs)
+                cv2.putText(frame, timer, org, font,
+                            fontScale, color, thickness, cv2.LINE_AA)
+                imgencode = cv2.imencode('.jpg', frame)[1]
 
-            # base64 encode
-            stringData = base64.b64encode(imgencode).decode('utf-8')
-            b64_src = 'data:image/jpg;base64,'
-            stringData = b64_src + stringData
+                # base64 encode
+                stringData = base64.b64encode(imgencode).decode('utf-8')
+                b64_src = 'data:image/jpg;base64,'
+                stringData = b64_src + stringData
 
-            # emit the frame back
-            emit('response_back', stringData)
-            chara["t"]-=1
-            if chara["t"]==1:
-                chara["flag"]=1
-            else:
+                # emit the frame back
+                emit('response_back', stringData)
+                chara["t"]-=1
+                if chara["t"]==1:
+                    chara["flag"]=1
+                else:
+                    pass
+            except:
                 pass
-
 
 
         if chara["flag"]==1:
             scr = cv2.imread('blank.jpg')
 
             if len(chara["l"]) != 0:
-                sample_sign_1 = show_display(scr, l)
+                sample_sign_1 = show_display(scr, chara["l"])
                 sample = get_text(sample_sign_1)
                 sample_sign = cv2.putText(sample[0], sample[1], org, font,
                                           fontScale, color, thickness, cv2.LINE_AA)
@@ -377,12 +379,7 @@ def image(data_image):
             pass
     else:
         pass
-'''@app.route('/video_feed',methods=['GET','POST'])
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-'''
+
 
 if __name__ == '__main__':
     #app.run(debug=True)
